40c5abb8a7fbb2e4944651429d11ebfd679fcab7
Jil Franco <jil.franco@aol.com>
2018-01-19 22:54:35 -0500

    added unity project

---
+++ Assets/Scripts/KeyboardMovement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    private float moveX;
+    private float moveY;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        CheckKeyInput();
+        MovePlayer();
+	}
+
+    private void CheckKeyInput()
+    {
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+    }
+
+    private void MovePlayer()
+    {
+        this.transform.Translate(new Vector3(moveX * moveSpeed, moveY * moveSpeed, 0));
+    }
+}
---
+++ Assets/Scripts/WallCollision.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCollision : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D wallCollision)
+    {
+        Debug.Log("Enter, better kill em");
+    }
+
+}
