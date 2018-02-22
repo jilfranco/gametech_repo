373824ecf0256cc82729bae872216f96440e2078
Jil <jil.franco@aol.com>
2018-02-22 12:21:09 -0500

    small stuff, added cam script and changed gravity of player from 1 to 2

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+	public Transform target;
+	public float followSpeed;
+
+	private float zPos;
+
+	void Start()
+	{
+		zPos = transform.position.z;
+	}
+
+	void FixedUpdate()
+	{
+		MoveCamera();
+	}
+
+	private void MoveCamera()
+	{
+		Vector3 newPos = Vector3.Lerp(transform.position, target.position, Time.deltaTime * followSpeed);
+		newPos.z = zPos;
+		transform.position = newPos;
+	}
+}
