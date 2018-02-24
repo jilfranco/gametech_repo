2d929e23322a33e42cb6b10e2adbdd9f11784e3a
Jil Franco <jillian.franco@mymail.champlain.edu>
2018-02-24 14:39:04 -0500

    started integrating code from class

---
+++ Assets/Scripts/PlatformManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformManager : MonoBehaviour
+{
+    public static PlatformManager PlatformManagerInstance;
+    public List<GameObject> platformList;
+    public GameObject platformCollider;
+
+    private void Awake()
+    {
+        PlatformManagerInstance = this;
+    }
+
+    void Start()
+    {
+        InitializePlatforms();
+    }
+
+    private void InitializePlatforms()
+    {
+        for (int i = 0; i < platformList.Count; i++)
+        {
+            GameObject platform = platformList[i];
+            Vector2 platformPosition = Vector2.zero;
+
+            if (i !=0)
+            {
+                GameObject previousPlatform = platformList[i - 1];
+                float previousPlatformXPos = previousPlatform.transform.localPosition.x;
+                float previousPlatformWidth = previousPlatform.GetComponent<Collider2D>().bounds.size.x / 2;
+
+                float newXPos = previousPlatformXPos + previousPlatformWidth;
+                platformPosition.x = newXPos;
+            }
+
+            platform.transform.localPosition = platformPosition;
+        }
+
+        SetCollider();
+    }
+
+    private void MovePlatform(GameObject platform)
+    {
+        Vector2 platformPosition = Vector2.zero;
+        GameObject previousPlatform = platformList[platformList.Count - 1];
+
+        float previousPlatformXPos = previousPlatform.transform.localPosition.x;
+        float previousPlatformWidth = previousPlatform.GetComponent<Collider2D>().bounds.size.x / 2;
+
+        platform.GetComponent<PlatformScript>().InitializePlatformTypes();
+
+        float newXPos = previousPlatformXPos + previousPlatformWidth;
+        platformPosition.x = newXPos;
+
+        platform.transform.localPosition = platformPosition;
+    }
+
+    private void SetCollider()
+    {
+        float colliderSize = 0f;
+        for (int i = 0; i < platformList.Count; i++)
+        {
+            GameObject platform = platformList[i];
+            colliderSize += platform.GetComponent<Collider2D>().bounds.size.x;
+        }
+    }
+}
---
+++ Assets/Scripts/PlatformScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour
+{
+    public GameObject[] platformTypes;
+
+    private void Awake()
+    {
+        InitializePlatformTypes();
+    }
+
+    public void InitializePlatformTypes()
+    {
+        RandomPlatformType();
+    }
+
+    private void RandomPlatformType()
+    {
+        int randomNumber = UnityEngine.Random.Range(0, platformTypes.Length);
+        GameObject platformType = platformTypes[randomNumber];
+    }
+}
