343918f13a684c7057c1d2fa2ac7e0b506de00da
Jil Franco <jillian.franco@mymail.champlain.edu>
2018-04-08 12:32:32 -0400

    made it so you can hover and select a piece. need to work on moving the piece and highlighting it when it's selected

---
+++ Assets/Scripts/ClickScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickScript : MonoBehaviour
+{
+    [SerializeField] private Material hoverMaterial;
+    [SerializeField] private Material selectedMaterial;
+    private Material defaultMaterial;
+
+    private void Awake()
+    {
+        defaultMaterial = GetComponent<SpriteRenderer>().sharedMaterial;
+    }
+
+    private void OnMouseOver()
+    {
+        GetComponent<SpriteRenderer>().material = hoverMaterial;
+    }
+
+    private void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().material = selectedMaterial;
+        GetComponent<Collider2D>().enabled = false;
+        Debug.Log("mouse down");
+    }
+
+    private void OnMouseExit()
+    {
+        GetComponent<SpriteRenderer>().material = defaultMaterial;
+    }
+}
--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -1,16 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class GridManager : MonoBehaviour {
+public class GridManager : MonoBehaviour
+{
+    public static GridManager gridManagerRef;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public List<GameObject> activeGridPieces;
+    public List<Vector2> originalPositions;
+
+    private void Awake()
+    {
+        gridManagerRef = this;
+        activeGridPieces = GameObject.FindGameObjectsWithTag("GridPiece").OrderBy(GetGridPieceIndex).ToList();
+    }
+
+    private int GetGridPieceIndex(GameObject gridPiece)
+    {
+        string pieceName = gridPiece.name;
+        int pieceNameAsNumber = int.Parse(pieceName);
+        return pieceNameAsNumber;
+    }
+
+    void Start()
+    {
+        ShuffleGridPieces();
+    }
+
+    private void ShuffleGridPieces()
+    {
+        for (int i = 0; i < activeGridPieces.Count; ++i)
+        {
+            Vector2 originalPos = activeGridPieces[i].transform.position;
+            originalPositions.Add(originalPos);
+        }
+
+        var positionCopy = new List<Vector2>(originalPositions);
+        for (int i = 0; i < activeGridPieces.Count; ++i)
+        {
+            int randomIndex = Random.Range(0, positionCopy.Count - 1);
+            activeGridPieces[i].transform.position = positionCopy[randomIndex];
+            positionCopy.RemoveAt(randomIndex);
+        }
+    }
+
+    private void MovePiece(GameObject firstGridPiece, GameObject secondGridPiece)
+    {
+        Vector2 ogPos = firstGridPiece.transform.position;
+        Vector2 newPos = secondGridPiece.transform.position;
+    }
+
+    public bool InOriginalSpot(GameObject gridPiece)
+    {
+        bool inOriginalSpot;
+        int index = GetGridPieceIndex(gridPiece);
+        Vector2 originalPosition = originalPositions[index];
+
+        if (Vector2.Distance(originalPosition, gridPiece.transform.position) < 0.05f)
+        {
+            inOriginalSpot = true;
+            gridPiece.GetComponent<Collider2D>().enabled = false;
+        }
+        else
+        {
+            inOriginalSpot = false;
+        }
+        return inOriginalSpot;
+    }
 }
